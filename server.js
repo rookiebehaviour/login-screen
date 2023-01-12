0129fc31e69ccd6de2acaa51ff4a468bcfa4504f
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-01-12 00:10:29 -0500

    added express-flash and express-session and initialized using passport

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
 
 const initializePassport = require("./passport-config");
-initializePassport(passport, (email) => {
-  users.find((user) => user.email === email);
-});
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 const users = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "Jacqueline" });
+  res.render("index.ejs", { name: req.user.name });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
